52421339da2e6d855dc72f6f24ea68c73c37e7bc
ARC4N3-3301 <69053040+ARC4N3-3301@users.noreply.github.com>
2020-08-05 18:02:23 +0530

    Update login_webcrawler_12r.py

--- webcrawler + login/login_webcrawler_12r.py
+++ webcrawler + login/login_webcrawler_12r.py
@@ -8,8 +8,8 @@ def lead_webcr(usr_id):
     page = '/login'
     url = link_cr + page
     credentials = {                                                              # enter your credentials, which you are going to push to the site to get to the leaderboard
-        'email': '12rbot.OP@gmail.com',                           # enter username
-        'password': '12rgobrrrr'                                  # enter password
+        'email': '<username>',                           # enter username
+        'password': '<password>'                                  # enter password
     }
     text = ''
     res = []
